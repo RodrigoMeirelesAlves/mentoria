d062c1735c53a136c069a681b8095a57513af75a
RodrigoMeirelesAlves <meirelesalve@gmail.com>
2023-03-06 21:45:22 -0300

    tabuada

---
+++ src/main/java/rodrigo/tabuada.java
@@ -0,0 +1,17 @@
+package rodrigo;
+
+public class tabuada {
+    public static void main(String[] args){
+
+        int tabuada = 2;
+
+        for ( int t = 1 ; t <= 10 ; t++){
+            System.out.println(t * tabuada);
+        }
+
+
+
+
+
+    }
+}
