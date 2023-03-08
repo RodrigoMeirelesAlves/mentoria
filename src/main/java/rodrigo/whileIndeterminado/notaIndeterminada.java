be406e5f19ea0e7bf05f2d36a2df92d7b0e82c69
RodrigoMeirelesAlves <meirelesalve@gmail.com>
2023-03-07 21:01:06 -0300

    Nota Indeterminada

---
+++ src/main/java/rodrigo/whileIndeterminado/notaIndeterminada.java
@@ -0,0 +1,27 @@
+package rodrigo.whileIndeterminado;
+
+import java.util.Scanner;
+
+public class notaIndeterminada {
+
+    public static void main(String[] args) {
+
+      //  Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja
+        //inválido e continue pedindo até que o usuário informe um valor válido.
+
+        Scanner entrada = new Scanner(System.in);
+         int nota = entrada.nextInt();
+
+        do {
+            System.out.println("Digite uma nota: ");
+            System.out.println("Nota Invàlida");
+            nota = entrada.nextInt();
+        } while (nota > 0 ^ nota <= 10 );
+
+        entrada.close();
+
+        System.out.println("Obrigado");
+
+    }
+
+}
--- src/main/java/rodrigo/whileIndeterminado/whileIndeterminado.java
+++ src/main/java/rodrigo/whileIndeterminado/whileIndeterminado.java
@@ -1,4 +1,4 @@
-package rodrigo;
+package rodrigo.whileIndeterminado;
 
 import java.util.Scanner;
 
